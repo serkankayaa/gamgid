5a89d897eb9c161486fbdbd0ce9943393023597c
serkankayaa <serkan.kaya@sys.com.tr>
2021-05-26 11:30:53 +0300

    initialized paging constants

---
+++ src/Gamgid.Paging.Const/PagingConsts.cs
@@ -0,0 +1,9 @@
+﻿namespace Gamgid.Paging.Const
+{
+    public static class PagingConsts
+    {
+        public const string Equal = "==";
+        public const string GreaterThan = ">=";
+        public const string SmallerThan = "<=";
+    }
+}
