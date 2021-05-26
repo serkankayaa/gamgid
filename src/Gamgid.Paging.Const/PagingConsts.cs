3e02620fd181a224a8ba123e0f13e65587884205
serkankayaa <srknnkaya@gmail.com>
2021-05-26 12:20:55 +0300

    modified constants

--- src/Gamgid.Paging.Const/PagingConsts.cs
+++ src/Gamgid.Paging.Const/PagingConsts.cs
@@ -2,8 +2,15 @@
 {
     public static class PagingConsts
     {
-        public const string Equal = "==";
-        public const string GreaterThan = ">=";
-        public const string LessThan = "<=";
+        public new const string Equals = "==";
+        public const string NotEquals = "!=";
+        public const string GreaterThan = ">";
+        public const string LessThan = "<";
+        public const string GreaterThanOrEqual = ">=";
+        public const string LessThanOrEqual = "<=";
+        public const string Contains = "/=";
+        public const string StartsWith = "_";
+        public const string NotContains = "!/=";
+        public const string NotStartsWith = "!_";
     }
 }
