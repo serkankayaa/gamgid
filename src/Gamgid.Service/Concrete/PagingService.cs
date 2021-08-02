f399ef1775f045e23d173cbfc79cf0f61d422952
serkankayaa <srknnkaya@gmail.com>
2021-08-02 16:48:02 +0300

    modified methods

--- src/Gamgid.Service/Concrete/PagingService.cs
+++ src/Gamgid.Service/Concrete/PagingService.cs
@@ -3,7 +3,6 @@ using Gamgid.Paging.Const;
 using Gamgid.Service.Abstract;
 
 using System;
-using System.Collections.Generic;
 using System.Linq;
 
 namespace Gamgid.Service.Concrete
@@ -17,30 +16,77 @@ namespace Gamgid.Service.Concrete
 
         public IQueryable<T> PageFilter<T>(IQueryable<T> data, GamgidModel gamgidModel) where T : class
         {
-            if(data == null)
+            if (data == null)
             {
                 throw new ArgumentNullException(nameof(data));
             }
 
-            if(gamgidModel == null)
+            if (gamgidModel == null)
             {
                 throw new ArgumentNullException(nameof(gamgidModel));
             }
 
-            IQueryable<T> result;
+            IQueryable<T> result = Paging(data, gamgidModel);
 
-            result = data.Skip(gamgidModel.PageSize * gamgidModel.PageNumber).Take(gamgidModel.PageSize);
-
-            if(gamgidModel.SearchCondition == PagingConsts.Contains)
-            {
-                result = result.Where(c => gamgidModel.SearchValue.Contains(c.GetType().GetProperty(gamgidModel.SearchKey).GetValue(c).ToString())).AsQueryable();
-            }
-            else if(gamgidModel.SearchCondition == PagingConsts.Equals)
-            {
-                result = result.Where(c => gamgidModel.SearchValue == c.GetType().GetProperty(gamgidModel.SearchKey).GetValue(c).ToString()).AsQueryable();
-            }
+            result = Filter(result, gamgidModel);
 
             return result;
         }
+
+        private static IQueryable<T> Paging<T>(IQueryable<T> data, GamgidModel gamgidModel) where T : class
+        {
+            return data.Skip(gamgidModel.PageSize * gamgidModel.PageNumber).Take(gamgidModel.PageSize);
+        }
+
+        private static IQueryable<T> Filter<T>(IQueryable<T> result, GamgidModel gamgidModel) where T : class
+        {
+            switch (gamgidModel.SearchCondition)
+            {
+                case PagingConsts.Contains:
+                    return result.Where(c => gamgidModel.SearchValue.Contains(c.GetType().GetProperty(gamgidModel.SearchKey).GetValue(c).ToString())).AsQueryable();
+                case PagingConsts.Equals:
+                    return result.Where(c => gamgidModel.SearchValue == c.GetType().GetProperty(gamgidModel.SearchKey).GetValue(c).ToString()).AsQueryable();
+                case PagingConsts.GreaterThan:
+                    CheckIsNumber(gamgidModel.SearchValue);
+
+                    return result.Where(c => int.Parse(gamgidModel.SearchValue) > int.Parse(c.GetType().GetProperty(gamgidModel.SearchKey).GetValue(c).ToString())).AsQueryable();
+                case PagingConsts.GreaterThanOrEqual:
+                    CheckIsNumber(gamgidModel.SearchValue);
+
+                    return result.Where(c => int.Parse(gamgidModel.SearchValue) >= int.Parse(c.GetType().GetProperty(gamgidModel.SearchKey).GetValue(c).ToString())).AsQueryable();
+                case PagingConsts.LessThan:
+                    CheckIsNumber(gamgidModel.SearchValue);
+
+                    return result.Where(c => int.Parse(gamgidModel.SearchValue) < int.Parse(c.GetType().GetProperty(gamgidModel.SearchKey).GetValue(c).ToString())).AsQueryable();
+                case PagingConsts.LessThanOrEqual:
+                    CheckIsNumber(gamgidModel.SearchValue);
+
+                    return result.Where(c => int.Parse(gamgidModel.SearchValue) <= int.Parse(c.GetType().GetProperty(gamgidModel.SearchKey).GetValue(c).ToString())).AsQueryable();
+                case PagingConsts.NotContains:
+                    return result.Where(c => !gamgidModel.SearchValue.Contains(c.GetType().GetProperty(gamgidModel.SearchKey).GetValue(c).ToString())).AsQueryable();
+                case PagingConsts.NotEquals:
+                    return result.Where(c => gamgidModel.SearchValue != c.GetType().GetProperty(gamgidModel.SearchKey).GetValue(c).ToString()).AsQueryable();
+                case PagingConsts.NotStartsWith:
+                    return result.Where(c => !gamgidModel.SearchValue.StartsWith(c.GetType().GetProperty(gamgidModel.SearchKey).GetValue(c).ToString())).AsQueryable();
+                case PagingConsts.StartsWith:
+                    return result.Where(c => gamgidModel.SearchValue.StartsWith(c.GetType().GetProperty(gamgidModel.SearchKey).GetValue(c).ToString())).AsQueryable();
+                default:
+                    return result;
+            }
+        }
+
+        #region Helper Methods
+
+        private static void CheckIsNumber(string value)
+        {
+            var checkIsGraterNumeric = int.TryParse(value, out int expectedNumber);
+
+            if (!checkIsGraterNumeric)
+            {
+                throw new InvalidOperationException($"Parameter value could be numeric value. Value is {value}");
+            }
+        }
+
+        #endregion
     }
 }
